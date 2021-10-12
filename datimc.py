3a1de77cca20deab7aa116f8c579f207bcae4f5b
Mark Joshwel <89562141+markjoshwel@users.noreply.github.com>
2021-10-12 13:18:53 +0000

    2.0.0: Internal functions

--- datimc.py
+++ datimc.py
@@ -1,5 +1,6 @@
 """
 datimc by Mark Joshwel
+----------------------
 
 This is free and unencumbered software released into the public domain.
 
@@ -26,3 +27,70 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 For more information, please refer to <http://unlicense.org/>
 """
+
+from random import randint
+from typing import Union, Tuple
+
+
+def h6_rgba(
+    h: str, alpha: bool = True
+) -> Union[Tuple[int, int, int], Tuple[int, int, int, int]]:
+    chi: str = "0123456789abcdef"
+    r: int = 0
+    g: int = 0
+    b: int = 0
+
+    rlh = h[::-1].lower()
+
+    # aabbggrr
+    # 87654321
+
+    for bpl, chr in enumerate(rlh[-2:]):
+        r += (16 ** bpl) * chi.index(chr)
+
+    for bpl, chr in enumerate(rlh[-4:-2]):
+        g += (16 ** bpl) * chi.index(chr)
+
+    for bpl, chr in enumerate(rlh[-6:-4]):
+        b += (16 ** bpl) * chi.index(chr)
+
+    if alpha:
+        a: int = 0
+
+        for bpl, chr in enumerate(rlh[:-6]):
+            a += (16 ** bpl) * chi.index(chr)
+
+        return (r, g, b, a)
+
+    else:
+        return (r, g, b)
+
+
+def int_b15(n: int) -> str:
+    res: str = ""
+    rem: int = 0
+    chi: str = "0123456789abcde"
+
+    while n > 0:
+        rem = n % 15
+        res = chi[rem] + res
+        n //= 15
+
+    return res
+
+
+def b15_int(h: str) -> int:
+    chi: str = "0123456789abcde"
+    res: int = 0
+
+    for plv, chr in enumerate(h[::-1]):
+        res += (15 ** plv) * chi.index(chr)
+
+    return res
+
+
+def gen(h: str = "") -> str:
+    for _ in range(6 - len(h)):
+        h += "0123456789abcdef"[randint(0, 15)]
+
+    return h
